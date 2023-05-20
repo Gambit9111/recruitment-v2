b150a04ab7622973424323faa7d7fb3c0bd11fb8
amag <bitgam911@gmail.com>
2023-05-21 00:11:12 +0200

    lets go nx

--- src/components/navigation/Header2.tsx
+++ src/components/navigation/Header2.tsx
@@ -5,6 +5,14 @@ import Footer from "./Footer";
 
 type Header2Props = {};
 
+const links = [
+  { href: "/", name: "Home" },
+  { href: "/about-us", name: "About Us" },
+  { href: "/vacancies", name: "Vacancies" },
+  { href: "/employers", name: "Employers" },
+  { href: "/apply", name: "Apply" },
+];
+
 const Header2: React.FC<Header2Props> = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -14,17 +22,34 @@ const Header2: React.FC<Header2Props> = () => {
 
   useEffect(() => {
     if (isOpen) {
-      document.body.style.overflow = 'hidden';
+      document.body.style.overflow = "hidden";
     } else {
-      document.body.style.overflow = 'unset';
+      document.body.style.overflow = "unset";
     }
   }, [isOpen]);
+
+  const variants = {
+    // @ts-ignore
+    open: (i) => ({
+      opacity: 1,
+      transition: {
+        delay: i * 0.15,
+      },
+    }),
+    closed: { opacity: 0 },
+  };
 
   // This function adds or removes a class from the body when the menu is open or closed
 
   return (
     <header className="sticky top-0 z-10 mx-4 flex items-center border-b border-color-white bg-color-black py-4 text-color-white">
-      <button className="z-30" onClick={handleToggle}>
+      <motion.button
+        className="z-30"
+        onClick={handleToggle}
+        initial={{ x: 100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ delay: 1, duration: 0.5 }}
+      >
         {isOpen ? (
           <svg
             width="32"
@@ -58,39 +83,42 @@ const Header2: React.FC<Header2Props> = () => {
             />
           </svg>
         )}
-      </button>
-      <div className="ml-4 flex h-12 w-12 items-center justify-center rounded-full border-[1.5px] border-color-white">
+      </motion.button>
+      <motion.div
+        className="ml-4 flex h-12 w-12 items-center justify-center rounded-full border-[1.5px] border-color-white"
+        initial={{ x: 100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ delay: 1.2, duration: 0.5 }}
+      >
         <p className="text-[0.55rem] font-medium uppercase tracking-wider">
           Engnr.
         </p>
-      </div>
+      </motion.div>
 
       <AnimatePresence>
         {isOpen && (
           <motion.div
-            className="fixed left-0 top-[5.05rem] z-20 h-[calc(100vh-5.05rem)] w-full bg-color-black overflow-auto"
+            className="fixed left-0 top-[5.05rem] z-20 h-[calc(100vh-5.05rem)] w-full overflow-auto bg-color-black"
             initial={{ y: "-100%" }}
             animate={{ y: 0 }}
             exit={{ y: "-100%" }}
             transition={{ duration: 0.5 }}
           >
-            {/* Add your dropdown menu items here */}
-            <ul className="space-y-8 pt-16 text-3xl font-light uppercase text-color-white/50 p-4">
-              <li>
-                <Link className="hover:text-color-yellow" href="/">Home</Link>
-              </li>
-              <li>
-                <Link className="hover:text-color-yellow" href="/about-us">About Us</Link>
-              </li>
-              <li>
-                <Link className="hover:text-color-yellow" href="/vacancies">Vacancies</Link>
-              </li>
-              <li>
-                <Link className="hover:text-color-yellow" href="/employers">Employers</Link>
-              </li>
-              <li>
-                <Link className="hover:text-color-yellow" href="/apply">Apply</Link>
-              </li>
+            <ul className="space-y-8 p-4 pt-16 text-3xl font-light uppercase text-color-white/50">
+              {links.map((link, i) => (
+                <motion.li
+                  key={link.href}
+                  custom={i}
+                  variants={variants}
+                  initial="closed"
+                  animate="open"
+                  exit="closed"
+                >
+                  <Link className="hover:text-color-yellow" href={link.href}>
+                    {link.name}
+                  </Link>
+                </motion.li>
+              ))}
             </ul>
             <Footer />
           </motion.div>
