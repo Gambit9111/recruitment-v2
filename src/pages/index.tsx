2097725dd796853bc99d17cdcb705427a1f44b84
Gambit9111 <bitgam911@gmail.com>
2023-05-22 11:27:12 +0200

    nu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,7 +41,7 @@ const Home: NextPage<HomeProps> = () => {
   const xPosition2 = useTransform(scrollY, [300, 10], ["70vw", "0vw"]);
   const yPosition3 = useTransform(scrollY, [300, 100], ["-100vh", "0vh"]);
 
-  const xPosition4 = useTransform(scrollY, [400, 10], ["0vw", "100vw"]);
+  const xPosition4 = useTransform(scrollY, [350, 10], ["0vw", "100vw"]);
   const opacityValue1 = useTransform(scrollY, [400, 10], [1, 0]);
 
   const [count, setCount] = useState(1);
@@ -203,7 +203,7 @@ const Home: NextPage<HomeProps> = () => {
             to Fortune 500 Companies.
           </motion.h2>
           <motion.div
-            style={{ opacity: opacityValue1 }}
+            style={{ x: xPosition4 }}
             initial={{ opacity: 0 }}
             animate={{ opacity: hasScrolled ? 1 : 0 }}
             transition={{ duration: 0.5 }}
