cc7f436d6b1e3b186812791682b8cd33208b8e8c
Gambit9111 <bitgam911@gmail.com>
2023-05-23 13:41:32 +0200

    added animated page transitions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,47 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import { Archivo } from "next/font/google";
+import { motion, AnimatePresence } from "framer-motion";
 
 const archivo = Archivo({ subsets: ["latin"] });
 
 import "@/styles/globals.css";
 
+import { useRouter } from "next/router";
+
+// @ts-ignore
 const MyApp: AppType = ({ Component, pageProps }) => {
+  const router = useRouter();
+
   return (
-    <main className={archivo.className}>
-      <Component {...pageProps} />
-    </main>
+    <AnimatePresence mode="wait">
+      <motion.main
+        key={router.route}
+        className={archivo.className}
+        initial="pageInitial"
+        animate="pageAnimate"
+        exit="pageExit"
+        // transition={{
+        //   duration: 5, // Duration for the entry animation
+        //   exit: 0.5, // Duration for the exit animation
+        // }}
+        variants={{
+          pageInitial: {
+            opacity: 0,
+            transition: { duration: 0.5 }, // Duration for the initial animation
+          },
+          pageAnimate: {
+            opacity: 1,
+            transition: { duration: 1 }, // Duration for the animate animation
+          },
+          pageExit: {
+            opacity: 0,
+            transition: { duration: 0.5 }, // Duration for the exit animation
+          },
+        }}
+      >
+        <Component {...pageProps} />
+      </motion.main>
+    </AnimatePresence>
   );
 };
 
